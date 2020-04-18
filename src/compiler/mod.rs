dddf841adf789e81cc0f65afb52903780c9fc582
Kai Mast <kaimast@cs.cornell.edu>
2020-04-17 18:34:06 -0700

    public generate compile error function

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -4,10 +4,63 @@ mod parser;
 
 pub use ast::*;
 
+use std::fmt::Debug;
+
 use lexer::Lexer;
 use parser::parse;
 
 pub use lexer::parse_indents;
+
+pub fn generate_compile_error<T>(input: &str, info: Option<(T, Span)>, e: &str) -> String
+        where T: Debug {
+    if let Some((token, span)) = info {
+        // Get interesting area
+        let mut pos = 0;
+        let mut start = 0;
+        let mut end = 0;
+        let mut it = input.chars();
+
+        let mut msg = String::default();
+
+        while pos < span.lo {
+            if it.next().unwrap() == '\n' {
+                start = pos+1;
+            }
+
+            pos += 1;
+        }
+
+        for _i in start..span.lo {
+            msg += " ";
+        }
+
+        while pos < span.hi {
+            it.next().unwrap();
+            pos += 1;
+            msg += "^";
+        }
+
+        msg += &format!("--- {:?}", e);
+
+        for c in it {
+            if c == '\n' {
+                end = pos;
+                break;
+            }
+            pos += 1;
+        }
+
+        if end == 0 {
+            end = pos;
+        }
+
+        format!("Got compile error at {:?} [{} to {}]:\n
+                |    {}\n\
+                |    {}", token, span.lo, span.hi, &input[start..end], msg)
+    } else {
+        format!("Got compile error: {:?}", e)
+    }
+}
 
 pub fn compile_string(input: &str) -> Program {
     #[ cfg(feature="verbose") ]
@@ -19,56 +72,7 @@ pub fn compile_string(input: &str) -> Program {
     match parse(lexer) {
         Ok(p) => { p }
         Err((info, e)) => {
-            match info {
-                Some((token, span)) => {
-                    // Get interesting area
-                    let mut pos = 0;
-                    let mut start = 0;
-                    let mut end = 0;
-                    let mut it = input.chars();
-
-                    let mut msg = String::default();
-
-                    while pos < span.lo {
-                        if it.next().unwrap() == '\n' {
-                            start = pos+1;
-                        }
-
-                        pos += 1;
-                    }
-
-                    for _i in start..span.lo {
-                        msg += " ";
-                    }
-
-                    while pos < span.hi {
-                        it.next().unwrap();
-                        pos += 1;
-                        msg += "^";
-                    }
-
-                    msg += &format!("--- {:?}", e);
-
-                    for c in it {
-                        if c == '\n' {
-                            end = pos;
-                            break;
-                        }
-                        pos += 1;
-                    }
-
-                    if end == 0 {
-                        end = pos;
-                    }
-
-                    panic!("Got compile error at {:?} [{} to {}]:\n\
-                            |    {}\n\
-                            |    {}", token, span.lo, span.hi, &input[start..end], msg);
-                }
-                None => {
-                    panic!("Got compile error: {:?}", e);
-                }
-            }
+            panic!(generate_compile_error(input, info, e));
         }
     }
 }
